14d8ad27f7549628a8be48526638a491db17d96a
OugaskeDev <RyanHOugaske@gmail.com>
2024-11-25 16:12:16 -0300

    first commit

---
+++ Documents/OrientadoAoObjeto/Projeto_Final/Projeto_Final_Ryan/src/projeto_final_ryan/Projeto_Final_Ryan.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Projeto_Final_Ryan;
+import Modelo.DadosPessoaisM;
+import Controle.DadosPessoaisC;
+import visao.CadastroEnderecoV;
+import visao.DadosPessoaisV;
+import visao.ConsultarDadosPessoaisV;
+import visao.ConsultaEnderecoV;
+
+
+public class Projeto_Final_Ryan {
+        
+    public static void main(String args[]){
+//       DadosPessoaisV telaregistro=new DadosPessoaisV();
+//       telaregistro.setVisible(true);
+//       ConsultarDadosPessoaisV telaconsulta =new ConsultarDadosPessoaisV();;
+//       telaconsulta.setVisible(true);
+//       CadastroEnderecoV telacadastro = new CadastroEnderecoV();
+//       telacadastro.setVisible(true);
+         ConsultaEnderecoV telaConsultarEnd = new ConsultaEnderecoV();
+         telaConsultarEnd.setVisible(true);
+            
+      
+    }
+    
+}
